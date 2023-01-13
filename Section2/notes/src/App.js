30979499dbfb6268bbcd1ee120921929a6f5471b
skipZero <skip@angerbunny.com>
2023-01-13 03:35:36 -0800

    added notes section/part

---
+++ Section2/notes/src/App.js
@@ -0,0 +1,16 @@
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App 
