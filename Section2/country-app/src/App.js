d88ed5d4f4703b6dce85057d3d866fb14a22ce83
skipZero <skip@angerbunny.com>
2023-01-19 01:09:21 -0800

    saving country app before changing to current section 2 exercises

--- Section2/country-app/src/App.js
+++ Section2/country-app/src/App.js
@@ -1,33 +1,58 @@
 import {useState, useEffect} from 'react';
-import Axios from 'axios';
+import axios from 'axios';
+
+const Filter = ({onChange, filter}) => {
+  
+  return (
+    <div>
+      <label>Find Countries: <input onChange={onChange} value={filter}/></label>
+    </div>
+  )
+}
+
+const Country = ({url, name}) => {
+  return (
+    <li>
+      <span><a href={`${url}name/${name}`}>{name}</a> </span>
+    </li>
+  )
+}
 
 function App() {
-  const {countries, setCountries} = useState([]);
+  const [filter, setFilter] = useState('');
+  const [countries, setCountries] = useState([])
 
-
-  const getCountries = async () => {
-    try {
-      await Axios.get('https://dark-sky.p.rapidapi.com/%7Blatitude%7D,%7Blongitude%7D')
-      // await Axios.get('https://restcountries.com/v3.1/alll')
-        // .then(res => res.json())
-        .then(res => console.log('RESP', res.data))
-        .then(res => setCountries(res.data))
-    } catch (err) {
-      console.log(`Error: ${err} while fetching countries`)
-    }
-  }
+  const baseUrl = 'https://restcountries.com/v3.1/'
 
 
   useEffect(() => {
-    getCountries()
-  }, [])
+    axios.get(`${baseUrl}all`)
+      .then(res => {
+        setCountries(res.data);
+      })
+      .then(res => {
+        console.log(countries)
+      })
+  },[]);
 
+  const onChange = () => {
+    return;
+  }
 
+  console.log('countries', countries)
   return (
     <div className="App">
-
+      <Filter value={filter} onChange={onChange}/>
+      {/* {countries.length > 10 ?
+      'Too many matches, please add more characters' : */
+      countries.map((country) => {
+        return <Country name={country.name.common} url={baseUrl} />       
+      })}
     </div>
   );
 }
 
 export default App;
+
+
+// await Axios.get('https://dark-sky.p.rapidapi.com/%7Blatitude%7D,%7Blongitude%7D')
