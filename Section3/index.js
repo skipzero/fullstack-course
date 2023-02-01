765a653537e078e3616a671c995c781ef237c72a
Skip McBride <skip@angerbunny.com>
2023-02-01 02:54:20 -0800

    notes app update;;

--- Section3/index.js
+++ Section3/index.js
@@ -26,6 +26,23 @@ app.get('/api/notes', (request, response) => {
   response.json(notes)
 })
 
+app.get('/api/notes/:id', (request, response) => {
+  const id = parseInt(request.params.id);
+  console.log('id', id)
+  const note = notes.find(note => note.id === id);
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  notes = notes.filter(note => note.id !== id)
+  res.status(204).end();
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
