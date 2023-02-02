8ba139c6bee8e1c132fb10bad7804c39a10ea8c1
skipZero <skip@angerbunny.com>
2023-02-01 16:38:51 -0800

    exercises 3.1-3.8, TODO: fix dup names getting thru

--- Section3/part3/index.js
+++ Section3/part3/index.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
-app.use(express.json())
+// create custom token for morgan logger
+morgan.token('body', (req, res) => {
+  console.log('BODY', req.body)
+  return JSON.stringify(req.body)
+})
 
-const people = require('./data')
+// we add our token to the output string
+const output = ':method :url :status :res[content-length] - :response-time ms :body';
 
+app.use(express.json()) // JSON parser
+app.use(morgan(output)) // Logger, using custom token (body)
+
+// get our data from the hardcoded file
+let people = require('./data')
+
+// get all entries
 app.get('/api/people', (req, res) => {
-  console.log('RES', req.headers)
   res.send(people)
 })
 
+// get a single entry
 app.get('/api/people/:id', (req, res) => {
-  console.log('GET', req.params)
   const id = parseInt(req.params.id);
   const person = people.find(person => person.id === id)
-  console.log('RES', person, req)
   if (person) {
     res.send(person)
   } else {
@@ -22,14 +33,50 @@ app.get('/api/people/:id', (req, res) => {
   }
 })
 
+// Add entry
+app.post('/api/people', (req, res) => {
+  const body = req.body;
+
+  const id = () => {
+    return Math.floor(Math.random() * 10000)
+  }
+
+  // Throw error if no name or number sent
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'name & number needed'
+    })
+  } 
+  
+  // TODO: fix so it doesn't post if names dup
+  people.filter(person => {
+    console.log('POST ppl', person.name, body.name)
+    console.log('person', person)
+    if (person.name.toLowerCase() === body.name.toLowerCase()) {
+      return res.status(400).json({
+        error: 'names must be unique'
+      })
+    }
+  })
+  const entry = {
+    id: id(),
+    name: body.name,
+    number: body.number,
+  }
+  people = people.concat(entry)
+  res.json(entry)
+  
+})
+
+// Delete a single entry by id
 app.delete('/api/people/:id', (req, res) => {
   const id = parseInt(req.params.id)
   person = people.filter(person => person.id !== id)
-  console.log('delete', person, people)
   res.status(204).end();
 
 })
 
+// our info page
 app.get('/info', (req, res) =>{
   const entries = people.length;
   res.send(`Phonebood has entries for ${entries} people <br> ${new Date()}`)
