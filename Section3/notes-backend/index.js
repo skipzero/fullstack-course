740840ed98d196e1a99907ab6f0f373e36e8f069
skipZero <skip@angerbunny.com>
2023-02-04 00:00:14 -0800

    working on notes app

--- Section3/notes-backend/index.js
+++ Section3/notes-backend/index.js
@@ -12,30 +12,31 @@ morgan.token('body', (req, res) => {
 const output = ':method :url :status :res[content-length] - :response-time ms :body';
 
 app.use(express.json()) // JSON parser
+app.use(express.static('build')) // Serve our static files here...
 app.use(morgan(output)) // Logger, using custom token (body)
 app.use(cors()); // CORS Module for cross browser support
 
 // get our data from the hardcoded file
-let people = require('./data')
+let notes = require('./data')
 
 // get all entries
-app.get('/api/people', (req, res) => {
-  res.send(people)
+app.get('/api/notes', (req, res) => {
+  res.send(notes)
 })
 
 // get a single entry
-app.get('/api/people/:id', (req, res) => {
+app.get('/api/notes/:id', (req, res) => {
   const id = parseInt(req.params.id);
-  const person = people.find(person => person.id === id)
-  if (person) {
-    res.send(person)
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    res.send(note)
   } else {
     res.status(404).end();
   }
 })
 
 // Add entry
-app.post('/api/people', (req, res) => {
+app.post('/api/notes', (req, res) => {
   const body = req.body;
 
   const id = () => {
@@ -43,46 +44,46 @@ app.post('/api/people', (req, res) => {
   }
 
   // Throw error if no name or number sent
-  if (!body.name || !body.number) {
+  if (!body.content) {
     return res.status(400).json({
-      error: 'name & number needed'
+      error: 'Content needed; no blank notes'
     })
   } 
   
   // TODO: fix so it doesn't post if names dup
-  people.filter(person => {
-    if (person.name.toLowerCase() === body.name.toLowerCase()) {
+  notes.filter(note => {
+    if (note.content.toLowerCase() === body.content.toLowerCase()) {
       return res.status(400).json({
-        error: 'names must be unique'
+        error: 'Content must be unique'
       }).end()
     } 
   })
   const entry = {
     id: id(),
-    name: body.name,
-    number: body.number,
+    content: body.content
   }
-  people = people.concat(entry)
+  notes = notes.concat(entry)
   res.json(entry)
   
 })
 
-// Delete a single entry by id
-app.delete('/api/people/:id', (req, res) => {
+// update a single entry by id
+app.put('/api/notes/:id', (req, res) => {
   const id = parseInt(req.params.id)
-  person = people.filter(person => person.id !== id)
-  res.status(204).end();
+  console.log('PUT...', req.body)
+  note = notes.find(note => note.id === id)
+  res.json(note);
 
 })
 
 // our info page
 app.get('/info', (req, res) =>{
-  const entries = people.length;
-  res.send(`Phonebood has entries for ${entries} people <br> ${new Date()}`)
+  const entries = notes.length;
+  res.send(`Notebook has entries for ${entries} notes <br> ${new Date()}`)
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
