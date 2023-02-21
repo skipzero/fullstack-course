c967294919424e40430c96c3c8cf545a80aac34c
skipZero <skip@angerbunny.com>
2023-02-21 13:40:50 -0800

    fixed mongo script

--- Section3/part3/mongo.js
+++ Section3/part3/mongo.js
@@ -4,54 +4,50 @@ if (process.argv.length<3) {
   console.log('give password as argument')
   process.exit(1)
 }
-console.log('PROcess', process.argv)
+
 const password = process.argv[2]
 
 const url =
   `mongodb+srv://bfalcon510:${password}@fsclusterzed.uh8mopv.mongodb.net/phonebookApp?retryWrites=true&w=majority`
 
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
+const connectMongo = async () => {
+  await mongoose.connect(url, {})
+  return mongoose;
+}
 
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: Number,
-})
+(async () => {
+  await connectMongo();
+  console.log('fired...')
+  mongoose.set('strictQuery',false)
+  const personSchema = new mongoose.Schema({
+    name: String,
+    number: Number
+  });
 
-const Person = mongoose.model('Person', personSchema)
-
-if (process.argv.length === 3) {
-  Person.find({})
-    .then(result => {
-      result.forEach(person => {
-        console.log(person)
-      })
-      mongoose.connection.close();
-    })
-} else {
+  const Person = mongoose.model('Person', personSchema)
   const name = process.argv[3]
   const number = process.argv[4]
   const person = new Person({
     name,
     number
-  })
+  });
+  debugger;
+  person.save()
+    .then(() => {
+      mongoose.connection.close();
+    });
+  // mongoose.connection.close();
 
-  person.save().then(result => {
-    console.log(`${name} saved to DB...`)
-  }).then(
-    mongoose.connection.close()
-  )
-}
+  if (process.argv.length === 3) {
+    Person.find({})
+      .then(result => {
+        result.forEach(person => {
+          console.log(person)
+        })
+      })
+      .then(() => {
+        mongoose.connection.close();
+      })
+  }
 
-
-Person.find({}).then(result => {
-  result.forEach(note => {
-    console.log(note)
-  })
-  mongoose.connection.close()
-})
-
-// note.save().then(result => {
-//   console.log('note saved!')
-//   mongoose.connection.close()
-// })
+})()
