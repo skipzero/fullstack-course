7bbe36d1eccc5871ba5aeb26f22b52cdbc56eb73
Skip McBride <skip@angerbunny.com>
2023-02-28 13:59:15 -0800

    temp

--- Section3/part3/mongo.js
+++ Section3/part3/mongo.js
@@ -1,61 +1,62 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length<3) {
+if (process.argv.length < 3) {
   console.log('give password as argument')
   process.exit(1)
 }
 
-mongoose.set('strictQuery',false)
+mongoose.set('strictQuery', false)
 
 const password = process.argv[2]
 
 const url =
   `mongodb+srv://bfalcon510:${password}@fsclusterzed.uh8mopv.mongodb.net/phonebookApp?retryWrites=true&w=majority`
 
-const connectMongo = async () => {
-  await mongoose.connect(url, {})
-  return mongoose;
-}
+mongoose.set('strictQuery', false)
 
-(async () => {
-  await connectMongo();
-  
-  const personSchema = new mongoose.Schema({
-    name: String,
-    number: Number
-  });
+  (async () => {
+    await connectMongo();
 
-  const Person = mongoose.model('Person', personSchema)
-  const name = process.argv[3]
-  const number = process.argv[4]
-  const person = new Person({
-    name,
-    number
-  });
-  
-  if (process.argv.length === 5) {
-    person.save()
-      .then(() => {
-        console.log(`added ${name} number ${number} to phonebook`)
-      })
-      .then(() => {
-        mongoose.connection.close();
-      });
-  }
+    const personSchema = new mongoose.Schema({
+      name: String,
+      number: Number
+    });
 
-  if (process.argv.length === 3) {
-    Person.find({})
-      .then(result => {
-        console.log('phonebook:')
-        result.forEach(person => {
-          if (person.name !== undefined) {
-            console.log(person.name, person.number)
-          }
+    const Person = mongoose.model('Person', personSchema)
+
+    if (process.argv.length === 3) {
+      mongoose.connect(url)
+      Person.find({})
+        .then(result => {
+          result.forEach(person => {
+            console.log(person)
+          })
+          mongoose.connection.close();
         })
-      })
-      .then(() => {
-        mongoose.connection.close();
-      })
-  }
+    } else {
+      const name = process.argv[3]
+      const number = process.argv[4]
+      const person = new Person({
+        name,
+        number
+      });
 
-})()
+      if (process.argv.length === 5) {
+        person.save()
+          .then(() => {
+            console.log(`added ${name} number ${number} to phonebook`)
+          })
+          .then(() => {
+            mongoose.connection.close();
+          });
+      }
+
+      mongoose.connect(url)
+      person.save().then(result => {
+        console.log(`${name} saved to DB...`)
+      }).then(
+        mongoose.connection.close()
+      )
+    }
+
+  })()
