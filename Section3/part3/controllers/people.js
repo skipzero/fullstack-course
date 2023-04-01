e64601b89bc03d1c03e6f2e970efb556a740c771
skipZero <skip@angerbunny.com>
2023-04-01 13:06:06 -0700

    commiting changes before starting personal site project

---
+++ Section3/part3/controllers/people.js
@@ -0,0 +1,1 @@
+const peopleRouter = require('express').Router()
---
+++ Section3/part3/utils/config.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+
+const PORT = process.env.PORT
+const URL = process.env.MONGO_URI
+
+
+modoule.exports = {
+  PORT,
+  URL
+}
