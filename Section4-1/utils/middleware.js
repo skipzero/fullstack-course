88d97f12670b1a1d2e57666969dc89d02c64386d
Skip McBride <skip@zerosquadron.com>
2023-11-29 23:03:10 -0800

    updated middleware.js

--- Section4-1/utils/middleware.js
+++ Section4-1/utils/middleware.js
@@ -1,7 +1,30 @@
-const { info } = require("./config");
+const { info, error } = require("./config");
 
-const requestLogger = (req, res, next) => {};
+const requestLogger = (req = { path, method, path, body }, res, next) => {
+  info(`Method: ${req.method}`);
+  info(`Path: ${req.path}`);
+  info(`Body: ${req.body}`);
+  info(`Info: -------------`);
+  next();
+};
+
+const unknownEndpoint = (_req, res) => {
+  res.status(404).send({ error: "unknown endpoint" });
+};
+
+const errorHandler = (error, _req, res, next) => {
+  error(error.message);
+
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformed ID" });
+  } else if (error.name === "ValidationError") {
+    return res.status(400).json(error.message);
+  }
+  next(error);
+};
 
 module.exports = {
   requestLogger,
+  unknownEndpoint,
+  errorHandler,
 };
